09db708507cc2536612fdde4d07eb8e706382fe2
phamtuanvtvn <phamtuanvtvn@gmail.com>
2014-02-09 09:46:32 +0100

    add LeapListener

--- com/papertoss/handler/LeapListener.java
+++ com/papertoss/handler/LeapListener.java
@@ -1,9 +1,12 @@
 package com.papertoss.handler;
 
+import com.jme3.scene.shape.Sphere;
 import com.leapmotion.leap.Controller;
 import com.leapmotion.leap.Finger;
+import com.leapmotion.leap.FingerList;
 import com.leapmotion.leap.Frame;
 import com.leapmotion.leap.Gesture;
+import com.leapmotion.leap.Hand;
 import com.leapmotion.leap.Listener;
 import com.leapmotion.leap.Vector;
 import com.papertoss.display.canvas.Canvas3D;
@@ -47,6 +50,30 @@ public class LeapListener extends Listener {
         		Vector vec = finger.tipPosition();
         		sphere.setPosition(vec.getX(), vec.getY(), vec.getZ());
         	}
+        }
+        
+        if (!frame.hands().isEmpty()) {
+            // Get the first hand
+            Hand hand = frame.hands().get(0);
+
+            // Check if the hand has any fingers
+            FingerList fingers = hand.fingers();
+            if (!fingers.isEmpty()) {
+                // Calculate the hand's average finger tip position
+                Vector avgPos = Vector.zero();
+                for (Finger finger : fingers) {
+                    avgPos = avgPos.plus(finger.tipPosition());
+                }
+                avgPos = avgPos.divide(fingers.count());
+                System.out.println("Hand has " + fingers.count()
+                                 + " fingers, average finger tip position: " + avgPos);
+            }
+
+            // Get the hand's sphere radius and palm position
+            System.out.println("Hand sphere radius: " + hand.sphereRadius()
+                             + " mm, palm position: " + hand.palmPosition());
+            
+            sphere.setRadius(hand.sphereRadius());
         }
     }
 }
